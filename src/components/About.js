399f79c6289e71d41a7fa107458cc9bd9c83c822
Keagan Barnes <barnes.keagan9@gmail.com>
2019-12-24 12:05:35 -0700

    added github link

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,7 @@ const About = () => (
       <Button circular color='twitter' icon='twitter' size='large' href="https://twitter.com/KeaganBarnes12" target="_blank" />
       <Button circular color='google plus' icon='instagram' size='large' href="https://www.instagram.com/keaganbarnes/?hl=en" target="_blank" />
       <Button circular color='linkedin' icon='linkedin' size='large' href="https://www.linkedin.com/in/keaganbarnes/" target="_blank" />
+      <Button circular color='black' icon='github' size='large' href="https://github.com/KeaganBarnes" target="_blank" />
     </div>
   </center>
 );
