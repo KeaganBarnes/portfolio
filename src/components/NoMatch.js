b25941eb64f706fa1e65d04ed08a2d2a9a3d7d22
Keagan Barnes <barnes.keagan9@gmail.com>
2019-12-30 23:43:13 -0700

    edited 404 page

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -3,8 +3,14 @@ import React from 'react'
 const NoMatch = () => (
   <center>
     <h1> Error 404: Page Not Found </h1>
-    <img src='https://media0.giphy.com/media/jKYbItcN39zWYdoNwL/giphy.gif' alt=''/>
+    <img style={gif} src='https://media0.giphy.com/media/jKYbItcN39zWYdoNwL/giphy.gif' alt=''/>
   </center>
 )
+
+const gif = {
+  height: '600px',
+  width: '1200px',
+  borderRadius: '10px'
+};
 
 export default NoMatch 
